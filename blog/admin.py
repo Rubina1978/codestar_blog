dac1939a4280fce2b9af1c471cfb5899bff956b8
Rubina1978 <hrubesova@gmail.com>
2026-04-05 15:28:24 +0100

    add comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Post
 # Register your models here.
-admin.site.register(Post)  # allowes me to create, update and delete blog posts from admin panel.
+admin.site.register(Post)  # allowes me to create, update and delete blog posts from admin panel.p
