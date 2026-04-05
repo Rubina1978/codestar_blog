4f0c66d49459fcc08f86341274a9d9025f7a9d1b
Rubina1978 <hrubesova@gmail.com>
2026-04-05 16:06:08 +0100

    updated admin.py, make migrations and migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
+
 # Register your models here.
-admin.site.register(Post)  # allowes me to create, update and delete blog posts from admin panel.p
+admin.site.register(Post)  # allowes me to create, update and delete blog posts from admin panel.py
+admin.site.register(Comment)
