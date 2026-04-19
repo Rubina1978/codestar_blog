5df06d8524b7c1208d1aae91ef2129b1ba750b58
Rubina1978 <hrubesova@gmail.com>
2026-04-19 12:11:10 +0200

    add django summer note

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Post)  # allowes me to create, update and delete blog posts from admin panel.py
+# allowes me to create, update and delete blog posts from admin panel.py
+
+
 admin.site.register(Comment)
